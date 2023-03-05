d20e7d0b25585349c84964cba8e5b3935cfae546
Lama, Christopher <lamac@stthom.edu>
2023-03-05 08:43:03 -0600

    Removed unused functions from borrowed code files. Also improved code performance as I enabled shared (only to read) matrices A, U, L. They are calculated only once for the whole program, drastically improving code performance. Table is now printed. Still need to code for the rest of the tables.

--- LU.py
+++ LU.py
@@ -2,7 +2,6 @@
 # Special thanks to Dr. John Starner
 
 import MatrixGeneration
-import statistics as stats
 
 # Decomposition algorithm
 def DECOMP(A, n, SHOW_LU):
@@ -39,7 +38,7 @@ def DECOMP(A, n, SHOW_LU):
     for i in range(n):
         L[i][i] = 1
 
-    U = A.copy()
+    U = A.copy() # can be made multithread
 
     for j in range(0, n - 1):  # j is column and represents the diagonal element
         for i in range(j + 1, n):  # i is row
@@ -48,13 +47,6 @@ def DECOMP(A, n, SHOW_LU):
             # adjust the row
             for k in range(0, n):
                 U[i][k] = U[i][k] - (factor * U[j][k])
-
-    # print the matrices
-    if SHOW_LU:
-        print("U = ")
-        MatrixGeneration.MatPrint(U, n)
-        print("L = ")
-        MatrixGeneration.MatPrint(L, n)
 
     return U, L
 
@@ -137,128 +129,7 @@ def BACKWARD_SUB(y, n, U, SHOW_x):
         x[i] = ((-1 * sum_row) + y[i]) / U[i][i]  # calculate the unknown x value for the row
     x.append(0)  # boundary conditions
 
-    # print x
-    if SHOW_x:
-        MatrixGeneration.MatPrint(y, n)
     return x
 
 
-def TabPrint(n, header, Y_j, x, EV, ABS_ERR, REL_ERR):
 
-    """
-    Description:
-        Returns the table with the calculated X and Y points, the solution, exact solution, and absolute and relative errors.
-        Adapted from https://www.codegrepper.com/code-examples/python/how+to+create+table+format+python+console+output+without+library
-
-    Parameters:
-        Header: list
-            Header of the table.
-            ['k', 'Y_j points', 'Solution', 'Exact Solution', 'Absolute Error', 'Relative Error']
-
-        Y_j: list
-            Calculated list of discrete points on the channel. Calculated through B_VExact_Yj_GENERATE() function.
-
-        EV: list
-
-            Calculated list of the exact velocity at a particular discrete point. Calculated through B_VExact_Yj_GENERATE() function in MatrixGeneration.py.
-
-        ABS_ERR: list
-
-            Calculated list of the absolute error (difference between numerical solution and exact solution).
-            Calculated through SOLVE() function in LU.py
-
-        REL_ERR: list
-
-            Calculated list of the relative error (difference between absolute error and exact solution).
-            Calculated through SOLVE() function in LU.py
-
-        n: int
-            Size of matrix; user-given in main() function as well as in the Graphical User Interface in GUI.py
-
-        x: list
-            x vector calculated through BACKWARD_SUB() function in LU.py
-
-
-    Returns:
-        y: list
-            The y vector
-        """
-
-    tableString = ""  # used to store the table
-
-    b = "|{: ^8} | {: ^30} | {: ^30} | {: ^30} | {: ^30}|{: ^30}|".format(*header)  # header
-    tableString += b + '\n'  # put newline
-    tableString += ((len(b) + 2) * '-') + '\n'  # make division between header and body of table
-
-    # put contents of the line into chart
-    for i in range(n + 2):
-        tableString += "|{: ^8} | {: ^30} | {: ^30} | {: ^30} | {: ^30}| {: ^30}|". \
-                           format(i, Y_j[i], x[i], EV[i], ABS_ERR[i], REL_ERR[i]) + '\n'
-
-    tableString += ((len(b) + 2) * '-') + '\n'  # close the table
-
-    return tableString
-
-
-def SOLVE(A, n, GUI, l=0, deltaP=0, H=0, Nu=0, SHOW_LU=False, SHOW_table=False):
-    """
-    Description:
-        Solves the system of linear equations and graphs the results. Used both in compute_engine.py and GUI.py
-
-    Parameters:
-
-        A: list
-            The generated matrix (the system of linear equations).
-
-        n: int
-            Size of matrix; user-given in main() function as well as in the Graphical User Interface.
-
-        GUI: bool
-            determined whether the SOLVE() function is meant to be used for a GUI or not.
-
-        l: int
-            Length of the channel
-
-        deltaP: int
-            Change in pressure.
-
-        H: int
-            Radius of the channel.
-
-        Nu: int
-            Viscosity of the blood.
-
-        SHOW_LU: bool
-            Determines whether the L and U matrices should be displayed.
-
-        SHOW_table: bool
-            Determines whether the table should be displayed.
-
-        SHOW_LU: bool
-            Determines whether the resulting L and U matrices will be printed out.
-            """
-    if GUI:
-        B, EV, Y_j = MatrixGeneration.B_VExact_Yj_GENERATE(n, H, l, deltaP, Nu)
-    else:
-        # H = float(input("H = "))  # radius
-        # l = float(input("L = "))  # length of channel
-        # deltaP = float(input("Delta P = "))  # change in pressure
-        # Nu = float(input("Viscosity = "))  # viscosity
-        B, EV, Y_j = MatrixGeneration.B_VExact_Yj_GENERATE(n, H, l, deltaP, Nu)
-    U, L = DECOMP(A, n, SHOW_LU)
-    y = FORWARD_SUB(n, L, B, False)
-    x = BACKWARD_SUB(y, n, U, False)
-    Absolute_error = [0.0]
-    Relative_error = [0.0]
-    for i in range(1, n + 1):
-        Absolute_error.append(abs(x[i] - EV[i]))
-        Relative_error.append(abs(Absolute_error[i] / EV[i]))
-    Absolute_error.append(0.0)
-    Relative_error.append(0.0)
-    # make table
-    # table = TabPrint(n, ['k', 'Y_j points', 'Solution', 'Exact Solution',
-    #                      'Absolute Error', 'Relative Error'], Y_j,
-    #                  x, EV, Absolute_error, Relative_error)
-    # if SHOW_table:
-    #     print(table)
-    return x, Y_j, stats.mean(Absolute_error), stats.mean(Relative_error)
--- compute_engine.py
+++ compute_engine.py
@@ -1,32 +1,150 @@
 # Christopher Lama
 # Special thanks to Dr. Charles Thangaraj
+# Code borrowed and adapted from https://github.com/ChristopherL891123/Research--Code-Development-for-Flow-Simulation
 
 import LU
-import MatrixGeneration as m
-import matplotlib.pyplot as plt
-
-# console executable
-def engine(n:int,h:int,l:int,nu:int,deltaP:int):
-
-    """
-   Description:
-       Function call used to
-
-   Parameters:
-       none
-
-   Returns:
-       none
-   """
-
-    user_input = 'y'  # meant to be used as a switch that will end the program if the user does not want to make more plots
+import statistics as stats
+import MatrixGeneration
+import random as r
 
 
-    A = m.GENERATE(n)  # Generates the A matrix
+n = 1000
+A = MatrixGeneration.GENERATE(1000)
+U, L = LU.DECOMP(A, n, False)
+pressure_list = [] # keep track of normally distributed random values for pressure
+viscosity_list = [] # keep track of normally distributed random values for viscosity
 
-    x, y_points, avg_absolute, avg_relative = LU.SOLVE(A, n,False, l, deltaP, h, nu, False, SHOW_table=True) # generates x,y points and the table
+output = ["table1", "table2", "table3", "table4"]
 
 
-    return avg_absolute,avg_relative
 
-# engine()
+def GEN(n):
+    Matrix = []
+    # set up the matrix
+    for a in range(n):
+        Matrix.append([])
+        for b in range(n):
+            Matrix[a].append(0)
+
+    # insert values
+    i = 0
+    for j in range(0, n):
+        Matrix[j][j] = 2
+        if i == n - 1:
+            break
+        i = j + 1
+        Matrix[j][i] = -1
+        Matrix[i][j] = -1
+
+    return Matrix
+
+
+def engine(l:float, r:float, Delta_P:float, Nu:float):
+
+#  make UNIVERSAL VAR of MATRIX AND LU FACTORIZATION
+
+    global n
+    global A
+    global U
+    global L
+
+    # calculate values using formulas
+    Delta_Y = (2 * r) / (n + 1)
+    V_max = (Delta_P * r ** 2) / (2 * Nu * l)
+    B = [(Delta_Y ** 2 * (2 * V_max)) / (r ** 2) for i in range(n)]
+    EV = []
+    Y_j_LIST = []
+
+    for i in range(0, n + 2):
+        Y_j = -r + i * Delta_Y
+        Y_j_LIST.append(Y_j)
+        EV.append(V_max * (1 - (Y_j / r) ** 2))
+
+    # no need to generate A and calculate U,L as it will always be re-calculated every iteration of the program.
+    # For size 1000, these matrices will always have the same values.
+
+    y = LU.FORWARD_SUB(n, L, B, False)
+    x = LU.BACKWARD_SUB(y, n, U, False)
+
+    Absolute_error = [0.0]
+    Relative_error = [0.0]
+    for i in range(1, n + 1):
+        Absolute_error.append(abs(x[i] - EV[i]))
+        Relative_error.append(abs(Absolute_error[i] / EV[i]))
+    Absolute_error.append(0.0)
+    Relative_error.append(0.0)
+
+    return stats.mean(Absolute_error),stats.mean(Relative_error)
+
+
+# ---> Executable Code <---
+
+
+# Part One: Generation of values for Radius = 10 centimeters, Length = 0.1 meters
+
+#make this multithreaded
+
+def calc1():
+
+    count = 0
+
+    header = "|{: ^8} | {: ^30} | {: ^30} | {: ^30} | {: ^30}|".format('Iteration', 'Nu', 'P', 'Avg Absolute Error', 'Avg Relative Error')
+    table = ""
+    table += header + '\n'  # put newline
+    table += ((len(header) + 2) * '-') + '\n'  # make division between header and body of table
+
+    for i in range(10):
+        count += 1
+        Delta_P = r.randrange(1,1000) # unit: Pascals
+        pressure_list.append(Delta_P)
+        Nu = r.randrange(1,1000) # unit: Pa*s
+        viscosity_list.append(Nu)
+
+        avg_abs, avg_rel = engine(l=0.1, r=10, Delta_P=Delta_P, Nu=Nu) # unit of l: meters, unit of r: centimeters
+
+        # add row of values to table
+        table += "|{: ^8} | {: ^30} | {: ^30} | {: ^30} | {: ^30}|".format(count, Nu, Delta_P, avg_abs, avg_rel) + '\n'
+
+    # output[0] = table
+    print(table) #
+
+
+# do it again
+
+import time as t
+a = t.perf_counter()
+calc1()
+print(t.perf_counter() - a)
+
+# store tables in file
+#
+#
+# f = open("outputs.txt", "a")
+# f.write("*****TABLE 1*****\n\n")
+# f.write(output[0])
+# f.write("\n\n")
+# f.close()
+#
+#
+# f = open("outputs.txt", "a")
+# f.write("*****TABLE 2*****\n\n")
+# f.write(output[1])
+# f.write("\n\n")
+# f.close()
+#
+#
+# f = open("outputs.txt", "a")
+# f.write("*****TABLE 3*****\n\n")
+# f.write(output[3])
+# f.write("\n\n")
+# f.close()
+#
+#
+# f = open("outputs.txt", "a")
+# f.write("*****TABLE 4*****\n\n")
+# f.write(output[4])
+# f.write("\n\n")
+# f.close()
+#
+#
+#
