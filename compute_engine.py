b2729c99f836fa88ffd51d992c2be743585aab21
Lama, Christopher <lamac@stthom.edu>
2023-03-11 06:24:21 -0600

    Made universal function to compute table instead of having 4 different functions to do so; defined calc(). More arguments were added to the function such as length and radius.

--- compute_engine.py
+++ compute_engine.py
@@ -3,9 +3,9 @@
 # Code borrowed and adapted from https://github.com/ChristopherL891123/Research--Code-Development-for-Flow-Simulation
 # Code borrowed uses Length of artery in meters and Radius of artery is in centimeters
 
-import LU #borrowed
+import LU  # borrowed
 import statistics as stats
-import MatrixGeneration #borrowed
+import MatrixGeneration  # borrowed
 import random as r
 import threading
 import sys
@@ -15,15 +15,27 @@ print("*=*=*=*=* INITIALIZING VALUES *=*=*=*=*")
 n = 10
 A = MatrixGeneration.GENERATE(n)
 U, L = LU.DECOMP(A, n, False)
-pressure_list = [] # keep track of normally distributed random values for pressure
-viscosity_list = [] # keep track of normally distributed random values for viscosity
+pressure_list = []  # keep track of normally distributed random values for pressure
+viscosity_list = []  # keep track of normally distributed random values for viscosity
 output = ["table1", "table2", "table3", "table4"]
-progress_bar = "" # append all progress of threads and print this repeatedly to the screen
+progress_bar = ""  # append all progress of threads and print this repeatedly to the screen
 print("*=*=*=*=* DONE INITIALIZING VALUES *=*=*=*=*")
 
-def engine(l:float, r:float, Delta_P:float, Nu:float):
 
-    #make UNIVERSAL VAR of MATRIX AND LU
+def engine(l: float, r: float, Delta_P: float, Nu: float):
+    """
+
+    Args:
+        l: length of artery
+        r: radius of artery
+        Delta_P: pressure
+        Nu: viscosity
+
+    Returns:
+        Absolute_error: list with calculated absolute errors for the table
+    """
+
+    # make UNIVERSAL VAR of MATRIX AND LU
 
     global n
     global A
@@ -56,22 +68,22 @@ def engine(l:float, r:float, Delta_P:float, Nu:float):
     Absolute_error.append(0.0)
     Relative_error.append(0.0)
 
-    return stats.mean(Absolute_error),stats.mean(Relative_error)
+    return stats.mean(Absolute_error), stats.mean(Relative_error)
 
 
 # Part One: Generation of values for Radius = 10 centimeters, Length = 0.1 meters
 
-def calc1(outputs):
-
+def calc(outputs: list, index_writes: int, length: int, radius: int):
     global progress_bar
 
-    count = 0 # to keep track of iteration for table
+    count = 0  # to keep track of iteration for table
     # set up table
-    header = "|{: ^8} | {: ^30} | {: ^30} | {: ^30} | {: ^30}|".format('Iteration', 'Nu', 'P', 'Avg Absolute Error', 'Avg Relative Error')
+    header = "|{: ^8} | {: ^30} | {: ^30} | {: ^30} | {: ^30}|".format('Iteration', 'Nu', 'P', 'Avg Absolute Error',
+                                                                       'Avg Relative Error')
     table = ""
     table += header + '\n'  # put newline
     table += ((len(header) + 2) * '-') + '\n'  # make division between header and body of table
-    prg_count = 0 # for the progress bar; reset and prints "|" char every 1000 iterations
+    prg_count = 0  # for the progress bar; reset and prints "|" char every 1000 iterations
 
     for i in range(10000):
 
@@ -81,113 +93,18 @@ def calc1(outputs):
         prg_count += 1
 
         count += 1
-        Delta_P = r.randrange(1,1000) # unit: Pascals
+        Delta_P = r.randrange(1, 1000)  # unit: Pascals
         pressure_list.append(Delta_P)
-        Nu = r.randrange(1,1000) # unit: Pa*s
+        Nu = r.randrange(1, 1000)  # unit: Pa*s
         viscosity_list.append(Nu)
 
-        avg_abs, avg_rel = engine(l=0.1, r=10, Delta_P=Delta_P, Nu=Nu) # unit of l: meters, unit of r: centimeters
+        avg_abs, avg_rel = engine(l=0.1, r=10, Delta_P=Delta_P, Nu=Nu)  # unit of l: meters, unit of r: centimeters
 
         # add row of values to table
         table += "|{: ^8} | {: ^30} | {: ^30} | {: ^30} | {: ^30}|".format(count, Nu, Delta_P, avg_abs, avg_rel) + '\n'
 
-    outputs[0] = table
+    outputs[index_writes] = table
 
-# Part Two: Generation of values for Radius = 10 centimeters, Length = 0.1 meters
-def calc2(outputs):
-
-    global progress_bar
-
-    count = 0 # to keep track of iteration for table
-    # set up table
-    header = "|{: ^8} | {: ^30} | {: ^30} | {: ^30} | {: ^30}|".format('Iteration', 'Nu', 'P', 'Avg Absolute Error', 'Avg Relative Error')
-    table = ""
-    table += header + '\n'  # put newline
-    table += ((len(header) + 2) * '-') + '\n'  # make division between header and body of table
-    prg_count = 0
-    for i in range(10000):
-
-        if prg_count == 1000:
-            progress_bar += "|"
-            prg_count = 0
-        prg_count += 1
-
-        count += 1
-        Delta_P = r.randrange(1,1000) # unit: Pascals
-        pressure_list.append(Delta_P)
-        Nu = r.randrange(1,1000) # unit: Pa*s
-        viscosity_list.append(Nu)
-
-        avg_abs, avg_rel = engine(l=0.1, r=100, Delta_P=Delta_P, Nu=Nu) # unit of l: meters, unit of r: centimeters
-
-        # add row of values to table
-        table += "|{: ^8} | {: ^30} | {: ^30} | {: ^30} | {: ^30}|".format(count, Nu, Delta_P, avg_abs, avg_rel) + '\n'
-
-    outputs[1] = table
-
-def calc3(outputs):
-
-    global progress_bar
-
-    # to keep track of iteration for table
-    count = 0
-    # set up table
-    header = "|{: ^8} | {: ^30} | {: ^30} | {: ^30} | {: ^30}|".format('Iteration', 'Nu', 'P', 'Avg Absolute Error', 'Avg Relative Error')
-    table = ""
-    table += header + '\n'  # put newline
-    table += ((len(header) + 2) * '-') + '\n'  # make division between header and body of table
-    prg_count = 0
-    for i in range(10000):
-
-        if prg_count == 1000:
-            progress_bar += "|"
-            prg_count = 0
-        prg_count += 1
-
-        count += 1
-        Delta_P = r.randrange(1,1000) # unit: Pascals
-        pressure_list.append(Delta_P)
-        Nu = r.randrange(1,1000) # unit: Pa*s
-        viscosity_list.append(Nu)
-
-        avg_abs, avg_rel = engine(l=0.3, r=10, Delta_P=Delta_P, Nu=Nu) # unit of l: meters, unit of r: centimeters
-
-        # add row of values to table
-        table += "|{: ^8} | {: ^30} | {: ^30} | {: ^30} | {: ^30}|".format(count, Nu, Delta_P, avg_abs, avg_rel) + '\n'
-
-    outputs[2] = table
-
-def calc4(outputs):
-
-    global progress_bar
-
-    # to keep track of iteration for table
-    count = 0
-    # set up table
-    header = "|{: ^8} | {: ^30} | {: ^30} | {: ^30} | {: ^30}|".format('Iteration', 'Nu', 'P', 'Avg Absolute Error', 'Avg Relative Error')
-    table = ""
-    table += header + '\n'  # put newline
-    table += ((len(header) + 2) * '-') + '\n'  # make division between header and body of table
-    prg_count = 0
-    for i in range(10000):
-
-        if prg_count == 1000:
-            progress_bar += "|"
-            prg_count = 0
-        prg_count += 1
-
-        count += 1
-        Delta_P = r.randrange(1,1000) # unit: Pascals
-        pressure_list.append(Delta_P)
-        Nu = r.randrange(1,1000) # unit: Pa*s
-        viscosity_list.append(Nu)
-
-        avg_abs, avg_rel = engine(l=0.1, r=100, Delta_P=Delta_P, Nu=Nu) # unit of l: meters, unit of r: centimeters
-
-        # add row of values to table
-        table += "|{: ^8} | {: ^30} | {: ^30} | {: ^30} | {: ^30}|".format(count, Nu, Delta_P, avg_abs, avg_rel) + '\n'
-
-    outputs[3] = table
 
 # --> Run code <--
 
@@ -198,10 +115,10 @@ def calc4(outputs):
 # Print progress
 
 
-task1 = threading.Thread(target=calc1,  args=[output], name='calc1').start()
-task2 = threading.Thread(target=calc2, args=[output], name='calc2').start() # argument for function must be inside list
-task3 = threading.Thread(target=calc3, args=[output], name='calc3').start()
-task4 = threading.Thread(target=calc4, args=[output] , name='calc4').start()
+task1 = threading.Thread(target=calc, args=[output, 0, 0.1, 10], name='calc1').start()
+task2 = threading.Thread(target=calc, args=[output, 1, 0.1, 100], name='calc2').start()  # argument for function must be inside list
+task3 = threading.Thread(target=calc, args=[output, 2, 0.3, 10], name='calc3').start()
+task4 = threading.Thread(target=calc, args=[output, 3, 0.3, 100], name='calc4').start()
 
 # write outputs to file
 while True:
@@ -212,38 +129,32 @@ while True:
 
     if output[0] != "table1" and output[1] != "table2" and output[2] != "table3" and output[3] != "table4":
 
-        f = open("outputs.txt", "a")
-        f.write("*****TABLE 1*****\n\n LENGTH=10 CENTIMETERS AND RADIUS=0.1 METERS\n")
-        f.write(output[0])
-        f.write("\n\n")
-        f.close()
+        file = open("outputs.txt", "a")
+        file.write("*****TABLE 1*****\n\n LENGTH=10 CENTIMETERS AND RADIUS=0.1 METERS\n")
+        file.write(output[0])
+        file.write("\n\n")
+        file.close()
 
+        file = open("outputs.txt", "a")
+        file.write("*****TABLE 2*****\n\n LENGTH=10 CENTIMETERS AND RADIUS=1 METER\n")
+        file.write(output[1])
+        file.write("\n\n")
+        file.close()
 
-        f = open("outputs.txt", "a")
-        f.write("*****TABLE 2*****\n\n LENGTH=10 CENTIMETERS AND RADIUS=1 METER\n")
-        f.write(output[1])
-        f.write("\n\n")
-        f.close()
+        file = open("outputs.txt", "a")
+        file.write("*****TABLE 3*****\n\n LENGTH=30 CENTIMETERS AND RADIUS=0.1 METERS\n")
+        file.write(output[2])
+        file.write("\n\n")
+        file.close()
 
-
-        f = open("outputs.txt", "a")
-        f.write("*****TABLE 3*****\n\n LENGTH=30 CENTIMETERS AND RADIUS=0.1 METERS\n")
-        f.write(output[2])
-        f.write("\n\n")
-        f.close()
-
-
-        f = open("outputs.txt", "a")
-        f.write("*****TABLE 4*****\n\n LENGTH=30 CENTIMETERS AND RADIUS=1 METER\n")
-        f.write(output[3])
-        f.write("\n\n")
-        f.close()
+        file = open("outputs.txt", "a")
+        file.write("*****TABLE 4*****\n\n LENGTH=30 CENTIMETERS AND RADIUS=1 METER\n")
+        file.write(output[3])
+        file.write("\n\n")
+        file.close()
 
         if input("Exit(y/n): ").lower() == "y":
             sys.exit(0)
 
     else:
         continue
-
-
-
