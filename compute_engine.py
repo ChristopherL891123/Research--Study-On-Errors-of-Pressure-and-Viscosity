24c7588576f789ce441ce64acaea29f1fe6ddfc9
Lama, Christopher <lamac@stthom.edu>
2023-03-24 16:04:50 -0500

    Added plotting functionality to visualize errors.

--- compute_engine.py
+++ compute_engine.py
@@ -2,10 +2,12 @@
 # Special thanks to Dr. Charles Thangaraj
 # Code borrowed and adapted from https://github.com/ChristopherL891123/Research--Code-Development-for-Flow-Simulation
 # Code borrowed uses Length of artery in meters and Radius of artery is in centimeters
+import matplotlib.pyplot as plt
 
 import LU  # borrowed
 import statistics as stats
 import MatrixGeneration  # borrowed
+import matplotlib as m
 import random as r
 import threading
 import sys
@@ -17,6 +19,8 @@ A = MatrixGeneration.GENERATE(n)
 U, L = LU.DECOMP(A, n, False)
 pressure_list = []  # keep track of normally distributed random values for pressure
 viscosity_list = []  # keep track of normally distributed random values for viscosity
+rel_error_list = [0,0,0,0]
+abs_error_list = [0,0,0,0]
 output = ["table1", "table2", "table3", "table4"]
 progress_bar = ""  # append all progress of threads and print this repeatedly to the screen
 print("*=*=*=*=* DONE INITIALIZING VALUES *=*=*=*=*")
@@ -73,8 +77,10 @@ def engine(l: float, r: float, Delta_P: float, Nu: float):
 
 # Part One: Generation of values 
 
-def calc(outputs: list, index_writes: int, length: int, radius: int):
+def calc(outputs: list, index_writes: int, rel_write_index: int, abs_write_index: int, length: int, radius: int):
     global progress_bar
+    global rel_error_list
+    global abs_error_list
 
     count = 0  # to keep track of iteration for table
     # set up table
@@ -84,6 +90,9 @@ def calc(outputs: list, index_writes: int, length: int, radius: int):
     table += header + '\n'  # put newline
     table += ((len(header) + 2) * '-') + '\n'  # make division between header and body of table
     prg_count = 0  # for the progress bar; reset and prints "|" char every 1000 iterations
+
+    temp_rel_err = []
+    temp_abs_err = []
 
     for i in range(10000):
 
@@ -98,12 +107,19 @@ def calc(outputs: list, index_writes: int, length: int, radius: int):
         Nu = r.randrange(1, 1000)  # unit: Pa*s
         viscosity_list.append(Nu)
 
-        avg_abs, avg_rel = engine(l=0.1, r=10, Delta_P=Delta_P, Nu=Nu)  # unit of l: meters, unit of r: centimeters
+        avg_abs, avg_rel = engine(l=length, r=radius, Delta_P=Delta_P, Nu=Nu)  # unit of l: meters, unit of r: centimeters
+
+        temp_abs_err.append(avg_abs)
+        temp_rel_err.append(avg_rel)
 
         # add row of values to table
         table += "|{: ^8} | {: ^30} | {: ^30} | {: ^30} | {: ^30}|".format(count, Nu, Delta_P, avg_abs, avg_rel) + '\n'
 
+    rel_error_list[rel_write_index] = temp_rel_err
+    abs_error_list[abs_write_index] = temp_abs_err
+
     outputs[index_writes] = table
+
 
 
 # --> Run code <--
@@ -112,10 +128,10 @@ def calc(outputs: list, index_writes: int, length: int, radius: int):
 # each item of the list is appended to a file, item by item with individual markers and information about readings for pressure
 # and viscosity and their corresponding calculated errors.
 
-task1 = threading.Thread(target=calc, args=[output, 0, 0.1, 10], name='calc1').start() #for Radius = 10 centimeters, Length = 0.1 meters
-task2 = threading.Thread(target=calc, args=[output, 1, 0.1, 100], name='calc2').start() # for Radius = 10 centimeters, Length = 1 meter
-task3 = threading.Thread(target=calc, args=[output, 2, 0.3, 10], name='calc3').start() # for Radius = 30 centimeters, Length = 0.1 meters
-task4 = threading.Thread(target=calc, args=[output, 3, 0.3, 100], name='calc4').start() #for Radius = 30 centimeters, Length = 1 meter
+task1 = threading.Thread(target=calc, args=[output, 0, 0,0, 0.1, 10], name='calc1').start() #for Radius = 10 centimeters, Length = 0.1 meters
+task2 = threading.Thread(target=calc, args=[output, 1, 1,1  ,0.1, 100], name='calc2').start() # for Radius = 10 centimeters, Length = 1 meter
+task3 = threading.Thread(target=calc, args=[output, 2, 2,2 ,0.3, 10], name='calc3').start() # for Radius = 30 centimeters, Length = 0.1 meters
+task4 = threading.Thread(target=calc, args=[output, 3, 3,3 , 0.3, 100], name='calc4').start() #for Radius = 30 centimeters, Length = 1 meter
 
  # argument for function must be inside list
 
@@ -152,6 +168,12 @@ while True:
         file.write("\n\n")
         file.close()
 
+        for i in range(4):
+            plt.plot(abs_error_list[i])
+            plt.show()
+            # plt.plot(rel_error_list[i])
+            # plt.show()
+#TODO run 10 diamteres (choose range) and 10 lengths (100 tables) , save the plots
         if input("Exit(y/n): ").lower() == "y":
             sys.exit(0)
 
